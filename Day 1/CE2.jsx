66e373f0e0bf5f13cd4ca701a3fa98991ad6ac01
sibisugan18 <727723eucs231@skcet.ac.in>
2024-09-03 21:44:41 +0530

    Create CE2.jsx

---
+++ Day 1/CE2.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const World = () => {
+    return (
+        <div>
+            <p>This is World Functional Component</p>
+        </div>
+    );
+};
+
+export default World;
